b42640481b4d92e1810424c7d5d29a33eb4b73f0
CodeReviser <codereviser404@gmail.com>
2024-03-01 11:02:03 +0530

    minimal change in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,11 @@ const Header = ({account}) => {
   return (
     <>
     <div id='home' className='flex flex-col  items-center justify-center w-[100%] mx-2 mb-2'>
-      <div className='flex justify-start items-start w-full'>
+      <div className='flex flex-col justify-start items-start w-full'>
     <h2 className=" bg-slate-500 rounded-md p-2">
                 Account: {account.length > 13 ? `${account.slice(0, 13)}...` : account}
     </h2>
+
 
       </div>
       <div className='section_box  flex flex-col sm:flex-row items-center  p-4 m-4 justify-center' >
@@ -18,7 +19,7 @@ const Header = ({account}) => {
           Welcome to <span className='text-gradient  font-bold'>DECENTERALIZE BLOOD BANK </span> App
           </h2>
           <p className='text-gray-100 font-mono text-sm m-2 mt-6'>
-          NOTE:as this website should be only manage by the Hospitals but you are a user so i have given you access to all the features of the WEB3 but in real life it will be manage by hospitals
+          NOTE: <span className='font-bold text-xl underline'>USE SEPOLIA TEST NETWORK</span> as this website should be only manage by the Hospitals but you are a user so i have given you access to all the features of the WEB3 but in real life it will be manage by hospitals
           </p>
         </div>
         <div className='flex justify-center items-center'>
